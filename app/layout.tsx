36acf238dc40d6aaa988a258e2aefd34def6c861
Dark Waves Tech <89140466+darkwaves-ofc@users.noreply.github.com>
2024-11-24 23:01:37 +0530

    "Removed GeistSans and GeistMono font imports from layout.tsx"

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,17 +3,6 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { Montserrat, Playfair_Display } from "next/font/google"
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 export const metadata: Metadata = {
   title: "UNESCO Society - Mahinda Rajapaksha College Matara",
   description:
