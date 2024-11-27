a6e12b3b07e178361e16145ae3c9006929b82d92
Rishmika Sandanu <104628935+RishBroProMax@users.noreply.github.com>
2024-11-27 13:17:01 +0530

    New Commited By Rish bro

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,55 +4,10 @@ import "./globals.css";
 import { Montserrat, Playfair_Display } from "next/font/google"
 
 export const metadata: Metadata = {
-  title: "UNESCO Society - Mahinda Rajapaksha College, Matara",
+  title: "UNESCO Society - Mahinda Rajapaksha College Matara",
   description:
-    "Welcome to the official website of the UNESCO Society at Mahinda Rajapaksha College, Matara. Learn about our initiatives, cultural heritage programs, and contributions to global education.",
-  keywords: [
-    "UNESCO Society",
-    "Mahinda Rajapaksha College",
-    "Matara",
-    "Education",
-    "Cultural Heritage",
-    "Global Initiatives",
-    "Student Programs",
-    "Cultural Events",
-    "Sustainability",
-    "UNESCO Projects",
-  ],
-  authors: [
-    { name: "UNESCO Society - Mahinda Rajapaksha College, Matara" },
-  ],
-  viewport: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
-  charset: "UTF-8",
-  themeColor: "#004aad",
-  additionalMetaTags: [
-    {
-      name: "application-name",
-      content: "UNESCO Society - Mahinda Rajapaksha College",
-    },
-    {
-      name: "theme-color",
-      content: "#004aad", // Matches themeColor for consistency
-    },
-    {
-      name: "robots",
-      content: "index, follow", // Encourages search engines to index and follow links
-    },
-    {
-      name: "author",
-      content: "UNESCO Society, Member Rishmika Sandanu",
-    },
-    {
-      name: "rating",
-      content: "General", // Indicates content is suitable for all audiences
-    },
-    {
-      httpEquiv: "X-UA-Compatible",
-      content: "IE=edge", // Ensures compatibility with Internet Explorer edge mode
-    },
-  ],
+    "Official website of the UNESCO Society at Mahinda Rajapaksha College Matara",
 };
-
 const montserrat = Montserrat({
   subsets: ["latin"],
   variable: "--font-montserrat",
